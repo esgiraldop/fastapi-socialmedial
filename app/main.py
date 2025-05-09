36fffb6a5c243072fc56a4d444895a9765af6568
COL-AE-068 <col-ae-068@MacBookPro-de-AE-COL-10071.local>
2025-05-09 00:13:21 -0500

    added get posts with comments

--- app/main.py
+++ app/main.py
@@ -1,34 +1,7 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+
+from app.routers.post import router as post_router
 
 app = FastAPI()
 
-
-class UserPostIn(BaseModel):
-    body: str
-
-
-class UserPost(UserPostIn):
-    id: int
-
-
-post_table = {}
-
-
-@app.get("/")
-async def root():
-    return {"message": "Hello world"}
-
-
-@app.post("/", response_model=UserPost)
-async def create_post(post: UserPostIn):
-    data = post.model_dump()
-    last_record_id = len(post_table)
-    new_post = {**data, "id": last_record_id}
-    post_table[last_record_id] = new_post
-    return new_post
-
-
-@app.get("/post", response_model=list[UserPost])
-async def get_all_posts():
-    return list(post_table.values())
+app.include_router(post_router, prefix="/api")
