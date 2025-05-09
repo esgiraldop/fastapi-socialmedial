51031a9d349eab353980ca8ef0bc369da0f3224d
COL-AE-068 <col-ae-068@MacBookPro-de-AE-COL-10071.local>
2025-05-08 23:19:22 -0500

    added endpoints for creating post and listing posts

--- app/main.py
+++ app/main.py
@@ -1,9 +1,34 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
-@app.get('/')
+
+class UserPostIn(BaseModel):
+    body: str
+
+
+class UserPost(UserPostIn):
+    id: int
+
+
+post_table = {}
+
+
+@app.get("/")
 async def root():
     return {"message": "Hello world"}
 
-print("Application running in port 8000")
+
+@app.post("/", response_model=UserPost)
+async def create_post(post: UserPostIn):
+    data = post.model_dump()
+    last_record_id = len(post_table)
+    new_post = {**data, "id": last_record_id}
+    post_table[last_record_id] = new_post
+    return new_post
+
+
+@app.get("/post", response_model=list[UserPost])
+async def get_all_posts():
+    return list(post_table.values())
